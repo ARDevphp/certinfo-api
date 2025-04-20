78eb81e81e232cce0131b594c77807efe9d63a16
Abdulaziz <abdulzazmirzayev@gmail.com>
2025-04-20 12:50:27 +0500

    certinfo project

--- database/factories/CertificateFactory.php
+++ database/factories/CertificateFactory.php
@@ -2,13 +2,8 @@
 
 namespace Database\Factories;
 
-use App\Models\Certificate;
-use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Certificate>
- */
 class CertificateFactory extends Factory
 {
     public function definition(): array
@@ -23,7 +18,6 @@ class CertificateFactory extends Factory
             'practice' => fake()->text(1000),
             'certificate_protection' => fake()->text(700),
             'finish_course' => fake()->dateTime(),
-            'qr_code' => 'http://localhost:5173/certificates/' . 1,
             'created_at' => fake()->dateTime(),
         ];
     }
